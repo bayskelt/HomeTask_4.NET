ec7f1a34093300f7a4a051ed85be1ab47133d393
bayskelt <rinognom@gmail.com>
2024-04-03 01:34:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace HomeTask_4.NET
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write($"Введiть кiлькiсть елементiв списку: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] mas = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Введiть {i + 1} елемент списку: ");
+                mas[i] = int.Parse(Console.ReadLine());
+            }
+
+            int min = mas[0];
+            int max = mas[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                if (min > mas[i])
+                {
+                    min = mas[i];
+                }
+                if (max < mas[i])
+                {
+                    max = mas[i];
+                }
+            }
+
+            Console.Write($"Сума максимального та мiнiмального чисел дорiвнює {min + max}");
+        }
+    }
+}
